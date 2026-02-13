2dd7342b785ca79cbfd12627b64f306ce00a1fba
bruno-gia <b_gia@hotmail.com>
2026-02-13 18:10:36 -0300

    setup: instalacao de componentes e organizacao de pastas

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -1,4 +1,9 @@
+/* ============================= Includes ============================= */
+
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 
 void app_main(void)
 {
